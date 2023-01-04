e5e376ed0c34be8bc9d52ab6c3dbaf1353104705
Maaacs <msb@icomp.ufam.edu.br>
2023-01-04 18:55:34 -0400

    Box completa

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from vpython import *
 from time import *
 
-#cria box e mostra no navegador 
-floor=box(pos=vector(0,-5,0), length=10, height=.2, width=10)
-ceiling=box(pos=vector(0,5,0), length=10, height=.2, width=10)
-#segurar com botao direito ou esquerdo ao lado da caixa e arrastar para ver todos os lados
+# cria box e mostra no navegador 
+# segurar com botao direito ou esquerdo ao lado da caixa e arrastar para ver todos os lados
+
+floor = box(pos=vector(0,-5,0), length=10, width=10, height=.1) # piso
+ceiling = box(pos=vector(0,5,0), length=10, width=10, height=.1) # teto
+backwall = box(pos=vector(0,0,-5), length=10, width=.1, height=10) # parede traseira
+leftwall = box(pos=vector(-5,0,0), length=.1, width=10, height=10) # parede esquerda
+rightwall = box(pos=vector(5,0,0), length=.1, width=10, height=10) # parede direita
+
+
+
 while True:
     pass
