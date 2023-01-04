76671c21fea8c87b6cf28d2f37c926ae57e267f2
Maaacs <msb@icomp.ufam.edu.br>
2023-01-04 18:41:37 -0400

    Walls

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#biblioteca vpython
 from vpython import *
+from time import *
 
-#cria esfera e mostra no navegador 
-ball=sphere(color=color.cyan)
-#animação para trocar cor da bola
-sleep(5)
-ball.color=color.purple
+#cria box e mostra no navegador 
+floor=box(pos=vector(0,-5,0), length=10, height=.2, width=10)
+ceiling=box(pos=vector(0,5,0), length=10, height=.2, width=10)
+#segurar com botao direito ou esquerdo ao lado da caixa e arrastar para ver todos os lados
 while True:
     pass
