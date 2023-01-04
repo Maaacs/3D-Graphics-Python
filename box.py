abc946db2ba6d586571a57b3c98412ee423ca3a8
Maaacs <msb@icomp.ufam.edu.br>
2023-01-04 10:08:16 -0400

    Caixa 3D

---
+++ box.py
@@ -0,0 +1,9 @@
+#biblioteca vpython
+from vpython import *
+from time import *
+
+#cria box e mostra no navegador 
+mybox=box()
+#segurar com botao direito ou esquerdo ao lado da caixa e arrastar para ver todos os lados
+while True:
+    pass
