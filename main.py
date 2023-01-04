60ea1f3604d473cc592d37c7e81065264a9751d4
Maaacs <msb@icomp.ufam.edu.br>
2023-01-04 10:00:08 -0400

    Animando esfera

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from vpython import *
 
 #cria esfera e mostra no navegador 
-ball=sphere()
+ball=sphere(color=color.cyan)
+#animação para trocar cor da bola
+sleep(5)
+ball.color=color.purple
 while True:
     pass
