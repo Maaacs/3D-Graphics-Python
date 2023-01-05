a37348d97dce48449f37b762278d419192ab4df3
Maaacs <msb@icomp.ufam.edu.br>
2023-01-04 20:07:57 -0400

    Movimento Ball

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from time import *
 # cria box e mostra no navegador 
 # segurar com botao direito ou esquerdo ao lado da caixa e arrastar para ver todos os lados
 
-floor = box(pos=vector(0,-5,0), length=10, width=10, height=.1) # piso
-ceiling = box(pos=vector(0,5,0), length=10, width=10, height=.1) # teto
-backwall = box(pos=vector(0,0,-5), length=10, width=.1, height=10) # parede traseira
-leftwall = box(pos=vector(-5,0,0), length=.1, width=10, height=10) # parede esquerda
-rightwall = box(pos=vector(5,0,0), length=.1, width=10, height=10) # parede direita
+floor = box(pos=vector(0,-5,0), size=vector(10, .1, 10)) # piso # length, width, height
+ceiling = box(pos=vector(0,5,0), size=vector(10, .1, 10))  # teto
+backwall = box(pos=vector(0,0,-5), size=vector(10, 10, .1))  # parede traseira
+leftwall = box(pos=vector(-5,0,0), size=vector(.1,10, 10))  # parede esquerda
+rightwall = box(pos=vector(5,0,0), size=vector(.1, 10, 10))  # parede direita
 
 marble = sphere(radius=.75, color=color.purple)
 
