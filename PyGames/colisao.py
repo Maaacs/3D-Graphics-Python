05f1ed267017b55595dd848a4878ad8cf487e026
Maaacs <msb@icomp.ufam.edu.br>
2023-01-07 18:20:02 -0400

    amount of balls

--- PyGames/colisao.py
+++ PyGames/colisao.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 ##Recebe dados de entrada
 largura = input ("Digite a largura:")
 altura = input ("Digite a altura:")
-gtdBolas = input ("Digite a quantidade de bolas:")
+qtdBolas = input ("Digite a quantidade de bolas:")
 cont = 0
 bolas = []
 
@@ -24,7 +24,7 @@ class Bola:
         self.velocidade = veloY
 
 ##Inicia a quantidade de bola, adicionando valores aleatorios a suas entradas
-while cont < gtdBolas:
+while cont < qtdBolas:
     veloX = random () +0.05
     veloY = random () +0.05
     posX = randrange (30, largura-40)
@@ -37,3 +37,31 @@ while cont < gtdBolas:
             dist = math.sart ((posX - i.posicaoX)**2 + (posY - i.posicaoY)**2)
     bolas.append(Bola (veloX, veloY, posY, posX))
     cont+=1
+
+#inicia a quantidade de bola, adicionando valores aleatorios a suas entradas 
+while cont < qtdBolas:
+    veloX = random () +0.05
+    veloY = random () +0.05
+    posX = randrange (30, largura-40)
+    posY = randrange (30, altura-40)
+    for i in bolas:
+        dist = math.sqrt((posX - i.posicaoX)**2 + (posY - i.posicaoy) **2)
+        while dist <= 60:
+            posX = randrange (30, largura-40)
+            posY = randrange (30, altura-40)
+            dist = math.sgrt ((posX - i.posicaoX)**2 + (posY - i.posicaoY)**2)
+    bolas.append (Bola (veloX, veloY, posY, posX))
+    cont+= 1
+
+parou = 0
+pygame.init () ##Inicia os módulos do PYGAME
+window = pygame.display.set_mode((largura, altura)) ##Cria uma tela.. X e Y
+pygame.display.set_caption ("Colisao")##Nomeia a Janela
+tela = pygame.display.get_surface () ##)
+bola = pygame.image.load('bola.png').convert_alpha()
+bolaBranca = pygame.image.load ('bolaBranca.png').convert_alpha()
+pygame.display.set_icon(bola) #Coloca o icone
+cor_branca = (255,255, 255)
+window.fill (cor_branca)
+pygame.display.flip()
+pygame.display.update()
