d22e718d608b9b027436868a22fb363fcb201418
Maaacs <msb@icomp.ufam.edu.br>
2023-01-07 18:56:04 -0400

    Display

--- PyGames/colisao.py
+++ PyGames/colisao.py
@@ -1,4 +1,4 @@
-# -*- coding: cp1252 -
+#créditos: Luan Klein
 import pygame, time, math
 from random import *
 from pygame.locals import *
@@ -65,3 +65,69 @@ cor_branca = (255,255, 255)
 window.fill (cor_branca)
 pygame.display.flip()
 pygame.display.update()
+
+# Funçao calculadora de velocidades apos as colisões
+def calculaVelocidade (vx1, vy1, vx2, vy2, p1, p2):
+    projVx1 = (float ((p1*vx1 + p2*vy1)) / float((p1**2+p2**2))) * p1 ##Projeção da velocidade X da bola 1 em relação ao Eixo
+    projVy1 = (float ((p1*vx1 + p2*vy1)) / float((p1**2+p2**2))) * p2
+    
+    projVx2 = (float((p1*vx2 + p2*vy2)) / float((p1**2+p2**2))) * p1
+    projVy2 = (float((p1<vx2 + p2*vy2)) / float((p1**2+p2**2))) * p2
+    
+    vx1f = vx1 - projVx1 + projVx2 # Velocidade Inicial
+    vy1f = vy1 - projVy1 + projVy2 # velocidade da projeção + velocidade projeção 2
+    
+    vx2f = vx2 - projVx2 + projVx1
+    vy2f =  vy2 - projVy2 + projVy1
+
+    return vx1f, vy1f, vx2f, vy2f
+
+while True:
+    ##pygame .time .wait (1)
+    cont = 0
+    while cont < qtdBolas:
+        ##Veriffica se as bolas colidiram na parede
+        if bolas[cont].posicaoX+50 >= largura or bolas[cont].posicaoX <= 0:
+            bolas[cont].velocidadeX *= -1
+        
+        if bolas[cont].posicaoY+50 >= altura or bolas[cont].posicaoY <= 0:
+            bolas[cont].velocidadeY *= -1
+        i = cont + 1
+
+        while i < qtdBolas:
+            ##verifica se as bolas colidiram entre si
+            dist = math.sqrt((bolas[cont].posicaoX - bolas[i].posicaoX)**2 + (bolas[cont].posicaoY - bolas[i].posicaoY) **2)
+            if dist <= 50:
+                ##Se sim, chama a função para verificar as novas velocidades
+                p1 = bolas[cont].posicaoX - bolas[i].posicaoX
+                p2 = bolas[cont].posicaoY - bolas[i].posicaoY
+
+                vx1, vy1, vx2, vy2 = calculaVelocidade(bolas[cont].velocidadeX, bolas[cont].velocidadeY, bolas[i].velocidadeX, bolas[i].velocidadeY,p1,p2)
+                ##Chama os métodos implementados na classe bola para fazer mudanca de variavel
+                bolas[cont].mudaVelocidade(vx1, vy1)
+                bolas[i].mudaVelocidade (vx2, vy2)
+            i+= 1
+        cont+= 1
+    
+    ##Faz as bolinhas se mexerem
+    for i in bolas:
+        ##some com as bolinhas na posição velha
+        tela.blit(bolaBranca, (i.posicaoX, i.posicaoY))
+        i.posicaoX += i.velocidadeX
+        i.posicaoY += i.velocidadeY
+        ##Coloca ela na posição nova
+        tela.blit(bola, (i.posicaoX, i.posicaoY))
+    
+    pygame.display.flip()
+    pygame.display.update()
+    for event in pygame.event.get() :
+        ## Verifica se o usuario apertou a tecla Q
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit() ## Fecha a tela do pygame
+                parou = 1
+        if event.type == pygame.QUIT: ## Verifica se o usuario clicou no X vermelho para fechar
+            pygame.quit()
+            parou = 1
+    if parou == 1:
+        break 
